984923fa1f1bc7e53324d76b80cebaa4b3ad82d8
Morty135 <morty135135135@gmail.com>
2024-01-23 20:23:13 +0100

    added shaderes 4.5 version of glad

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ void processInput(GLFWwindow *window);
 int main()
 {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(width, height, "GLCEngine2.0", NULL, NULL);
